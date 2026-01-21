fda7af7b2cddeb926b76c83cb0e775c21225173f
unknown <laureanoezequielm@gmail.com>
2026-01-21 20:54:56 -0300

    change7

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,17 +1,11 @@
-import adapter from '@sveltejs/adapter-netlify'; // Cambia adapter-auto por adapter-netlify
+import adapter from '@sveltejs/adapter-netlify'; // <--- Asegúrate de que sea este
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto solo funciona en algunos entornos, mejor especificar netlify
-		adapter: adapter({
-            // si tu proyecto es puramente estático, puedes usar edge: false
-            edge: false,
-            split: false
-        })
+		adapter: adapter() 
 	}
 };
 
